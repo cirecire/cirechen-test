2695e0ee1e1f2972a297bdbd52f030c933f09c6f
cirecire <105835955+cirecire@users.noreply.github.com>
2026-08-31 15:27:05 +0800

    ch05: renderMath \$ escape + data uses \$11

--- math/math_data.js
+++ math/math_data.js
@@ -1147,7 +1147,7 @@ var MATH_DATA = [
       gx: 5,
       title: "Systems of Linear Equations",
       coverage: "g",
-      think: "You buy 3 apples and 2 bananas for $11. Your friend buys 2 apples and 3 bananas for $9. Can you find the price of each fruit?",
+      think: "You buy 3 apples and 2 bananas for \\$11. Your friend buys 2 apples and 3 bananas for \\$9. Can you find the price of each fruit?",
       subtopics: [
         { name: "方程组入门", vids: [0,1], pq: [] },
         { name: "代入法", vids: [2], pq: [0,1] },
